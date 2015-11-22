a00bc8b717a339ce987e58d8035d856f66db6034
Vaishnavi Galgali <vaishnavigalgali109@gmail.com>
2015-11-22 12:26:37 -0800

    Final submission of Assignment-3

--- TripPlanner.go
+++ TripPlanner.go
@@ -82,16 +82,18 @@ func PlanTrips(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
 	json.NewDecoder(req.Body).Decode(&request)
 	fmt.Println("IN PlanTrips")
 	
-	NearestLocation := getShortestRoute(request.Starting_from_location_id,request.Location_ids)
-	response.Best_route_location_ids= append(response.Best_route_location_ids,NearestLocation)
-	NewArray := RemoveFromArray(NearestLocation,request.Location_ids)
+	NewNearestLocation := getShortestRoute(request.Starting_from_location_id,request.Location_ids)
+	response.Best_route_location_ids= append(response.Best_route_location_ids,NewNearestLocation)
+	NewArray := RemoveFromArray(NewNearestLocation,request.Location_ids)
 	
 	for j:=0;len(response.Best_route_location_ids)!= len(request.Location_ids);j++{
-		NewNearestLocation := getShortestRoute(request.Location_ids[j],NewArray)
+		NewNearestLocation = getShortestRoute(NewNearestLocation,NewArray)
 		response.Best_route_location_ids= append(response.Best_route_location_ids,NewNearestLocation)
 		NewArray = RemoveFromArray(NewNearestLocation,NewArray)
 		
 	}
+	fmt.Println(len(response.Best_route_location_ids))
+	ReturnToStart(response.Best_route_location_ids[len(response.Best_route_location_ids)-1],request.Starting_from_location_id)
 	fmt.Println(total_distance)
 	fmt.Println(total_duration)
 	fmt.Println(total_estimate)
@@ -133,7 +135,60 @@ func PlanTrips(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
     	
 
 }
+func ReturnToStart(StartLocation string,EndLocation string){
+	    mongoDBDialInfo := &mgo.DialInfo{
+		Addrs:    []string{"ds043694.mongolab.com:43694"},
+		Timeout:  60 * time.Second,
+		Database: "locations",
+		Username: "Admin",
+		Password: "Admin123",
+	    }    
 
+	    session, err := mgo.DialWithInfo(mongoDBDialInfo)
+	    if err != nil {
+	        panic(err)
+	    }
+	    defer session.Close()
+
+	    session.SetMode(mgo.Monotonic, true)
+	    c := session.DB("locations").C("LocDetails")
+	    startresult := Data{}
+	    endresult := Data{}
+		err = c.Find(bson.M{"id": StartLocation}).One(&startresult)
+		if err != nil {
+			panic(err)}
+
+		err = c.Find(bson.M{"id": EndLocation}).One(&endresult)
+		if err != nil {
+			panic(err)}
+		start_latitude := startresult.Coordinate.Lat
+		start_longitude :=startresult.Coordinate.Lng	
+		resp, err := http.Get("https://sandbox-api.uber.com/v1/estimates/price?start_latitude="+start_latitude+"&start_longitude="+start_longitude+"&end_latitude="+endresult.Coordinate.Lat+"&end_longitude="+endresult.Coordinate.Lng+"&server_token=o0fHM9H2CMsFq2wOBD2gYuoAe1V0MTjs5pYYY191")
+		if err != nil {
+					// handle error
+					fmt.Println("Error")
+				}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+	
+    	var f interface{}
+  		json.Unmarshal(body,&f)
+    	//fmt.Println(error)
+    	mRes := f.(map[string]interface{})["prices"]
+
+    	mRes0 := mRes.([]interface{})[0]
+    	//fmt.Println(mRes0)
+
+    	estimate := mRes0.(map[string]interface{})["low_estimate"].(float64)
+    	duration := mRes0.(map[string]interface{})["duration"].(float64)
+    	distance := mRes0.(map[string]interface{})["distance"].(float64)
+
+   		total_estimate+=estimate
+	   	total_duration+=duration
+   		total_distance+=distance
+
+
+}
 func RemoveFromArray(Location string , Location_ids[] string) []string{
 	for i:=0;i<len(Location_ids);i++{
 		if Location_ids[i]==Location{
@@ -142,9 +197,6 @@ func RemoveFromArray(Location string , Location_ids[] string) []string{
 		}
 			
 
-	}
-	for j:=0;j<len(Location_ids);j++{
-		fmt.Println(Location_ids[j])
 	}
 	return Location_ids
 
@@ -183,6 +235,7 @@ func getShortestRoute(startLocation string , Location_ids[] string) string{
 	start_longitude :=result.Coordinate.Lng
 	nearestPlaceEstimateFromStart := 9999.00
 	nearestPlaceDurationFromStart := 9999.00
+	nearestPlaceDistanceFromStart := 9999.00
 	for i:=0;i<len(Location_ids);i++{
 		err = c.Find(bson.M{"id": Location_ids[i]}).One(&output)
 		if err != nil {
@@ -203,30 +256,35 @@ func getShortestRoute(startLocation string , Location_ids[] string) string{
     	mRes := f.(map[string]interface{})["prices"]
 
     	mRes0 := mRes.([]interface{})[0]
-    	//fmt.Println(mRes0)
+    	//fmt.Println(f)
 
     	estimate = mRes0.(map[string]interface{})["low_estimate"].(float64)
     	duration = mRes0.(map[string]interface{})["duration"].(float64)
     	distance = mRes0.(map[string]interface{})["distance"].(float64)
-
+    	//fmt.Println("Route",startLocation,Location_ids[i])
+    	//fmt.Println(estimate)
    		//fmt.Println("Low Estimate")
    		if estimate < nearestPlaceEstimateFromStart{
    			LocationId = Location_ids[i]
    			nearestPlaceEstimateFromStart = estimate
    			nearestPlaceDurationFromStart = duration
+   			nearestPlaceDistanceFromStart = distance
    		} 
    		if estimate == nearestPlaceEstimateFromStart{
    			if duration < nearestPlaceDurationFromStart{
    				LocationId = Location_ids[i]
 	   			nearestPlaceDurationFromStart = duration
-	
+	   			nearestPlaceEstimateFromStart = estimate
+				nearestPlaceDistanceFromStart = distance
    			}
    			   		}
    	
 	}
-	total_estimate+=estimate
-   	total_duration+=duration
-   	total_distance+=distance
+	fmt.Println(estimate)
+	fmt.Println(duration)
+	total_estimate+=nearestPlaceEstimateFromStart
+   	total_duration+=nearestPlaceDurationFromStart
+   	total_distance+=nearestPlaceDistanceFromStart
    	
 	return LocationId
 }
